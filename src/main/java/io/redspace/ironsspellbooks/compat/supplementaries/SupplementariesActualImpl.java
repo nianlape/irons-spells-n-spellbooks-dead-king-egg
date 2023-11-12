cf2961f7b62fa9e9fbd760940fc50d6d7233985b
iron431 <34083081+iron431@users.noreply.github.com>
2023-11-12 11:39:36 -0600

    testing map

--- src/main/java/io/redspace/ironsspellbooks/compat/supplementaries/SupplementariesActualImpl.java
+++ src/main/java/io/redspace/ironsspellbooks/compat/supplementaries/SupplementariesActualImpl.java
@@ -1,14 +1,20 @@
 package io.redspace.ironsspellbooks.compat.supplementaries;
 
 import io.redspace.ironsspellbooks.IronsSpellbooks;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.mehvahdjukaar.supplementaries.common.block.blocks.CandleHolderBlock;
 import net.mehvahdjukaar.supplementaries.common.block.blocks.LightUpBlock;
 import net.mehvahdjukaar.supplementaries.reg.ModRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.SectionPos;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CandleBlock;
 import net.minecraft.world.level.block.LanternBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.AttachFace;
@@ -16,21 +22,36 @@ import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class SupplementariesActualImpl implements ISupplementariesProxy {
 
-    public SupplementariesActualImpl(){
+    public SupplementariesActualImpl() {
         IronsSpellbooks.LOGGER.debug("Loading SupplementariesActualImpl");
     }
+
+    static final LazyOptional<Object2ObjectOpenHashMap<Block, BlockReplacement>> REPLACEMENT_MAP = LazyOptional.of(() -> new Object2ObjectOpenHashMap<>(
+            Map.of(
+                    Blocks.LANTERN, new BlockReplacement((blockState) -> blockState.getValue(LanternBlock.HANGING), (beforeState) -> ModRegistry.CANDLE_HOLDERS.get(null).get().defaultBlockState().setValue(CandleHolderBlock.FACE, AttachFace.CEILING).setValue(LightUpBlock.LIT, true), 1f),
+                    Blocks.SOUL_LANTERN, new BlockReplacement((blockState) -> blockState.getValue(LanternBlock.HANGING), (beforeState) -> ModRegistry.CANDLE_HOLDERS.get(DyeColor.LIGHT_BLUE).get().defaultBlockState().setValue(CandleHolderBlock.FACE, AttachFace.CEILING).setValue(LightUpBlock.LIT, true), 1f),
+                    Blocks.CANDLE, new BlockReplacement((blockState) -> true, (beforeState) -> ModRegistry.CANDLE_HOLDERS.get(null).get().defaultBlockState().setValue(CandleHolderBlock.CANDLES, beforeState.getValue(CandleBlock.CANDLES)).setValue(LightUpBlock.LIT, beforeState.getValue(CandleBlock.LIT)).setValue(CandleHolderBlock.FACING, Utils.random.nextBoolean() ? Direction.EAST : Direction.NORTH), .15f)
+            )
+    ));
+
     @Override
     public StructureTemplate.StructureBlockInfo handleStructureProcessor(@NotNull LevelReader level, @NotNull BlockPos jigsawPiecePos, @NotNull BlockPos jigsawPieceBottomCenterPos, @NotNull StructureTemplate.StructureBlockInfo blockInfoLocal, @NotNull StructureTemplate.StructureBlockInfo blockInfoGlobal, @NotNull StructurePlaceSettings settings, @Nullable StructureTemplate template) {
         //IronsSpellbooks.LOGGER.debug("SupplementariesActualImpl.handleStructureProcessor: {}", blockInfoGlobal.state.getBlock().getName().getString());
-        if (blockInfoGlobal.state.is(Blocks.LANTERN) && blockInfoGlobal.state.getValue(LanternBlock.HANGING)) {
-            //IronsSpellbooks.LOGGER.debug("SupplementariesActualImpl.handleStructureProcessor: replacing hanging lantern");
-            BlockState blockState = ModRegistry.CANDLE_HOLDERS.get(null).get().defaultBlockState().setValue(CandleHolderBlock.FACE, AttachFace.CEILING).setValue(LightUpBlock.LIT, true);
-            handleBlockReplacement(level, blockInfoGlobal.pos, blockState);
+        BlockReplacement replacement = REPLACEMENT_MAP.resolve().get().get(blockInfoGlobal.state.getBlock());
+        if (replacement != null) {
+            if (replacement.probability < 1f && Utils.random.nextFloat() > replacement.probability && replacement.match.test(blockInfoGlobal.state)) {
+                handleBlockReplacement(level, blockInfoGlobal.pos, replacement.replacement.apply(blockInfoGlobal.state));
+            }
         }
         return blockInfoGlobal;
     }
@@ -43,11 +64,12 @@ public class SupplementariesActualImpl implements ISupplementariesProxy {
         // if section index is < 0 we are out of bounds
         if (sectionIndex >= 0) {
             LevelChunkSection section = chunk.getSection(sectionIndex);
-            this.setBlock(section, pos, blocksState);
+            section.setBlockState(SectionPos.sectionRelative(pos.getX()), SectionPos.sectionRelative(pos.getY()), SectionPos.sectionRelative(pos.getZ()), blocksState);
+
         }
     }
 
-    private void setBlock(LevelChunkSection section, BlockPos pos, BlockState state) {
-        section.setBlockState(SectionPos.sectionRelative(pos.getX()), SectionPos.sectionRelative(pos.getY()), SectionPos.sectionRelative(pos.getZ()), state);
+    record BlockReplacement(Predicate<BlockState> match, Function<BlockState, BlockState> replacement,
+                            float probability) {
     }
 }
